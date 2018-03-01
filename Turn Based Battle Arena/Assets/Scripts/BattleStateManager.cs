b4326bc0620076e578aba038674149f2289ab9c4
Richard But <butr@uci.edu>
2018-02-28 16:14:46 -0800

    Decrease Jump + Added Prompts w/ Animation
    Jump strength decreased
    Prompts added with animation
    Prompts properly stop if game ends

--- Turn Based Battle Arena/Assets/Scripts/BattleStateManager.cs
+++ Turn Based Battle Arena/Assets/Scripts/BattleStateManager.cs
@@ -13,11 +13,26 @@ public class BattleStateManager : MonoBehaviour {
 
     public Text player1HealthText;
     public Text player2HealthText;
+
+    public float promptTime;
+    public float promptFadeDuration;
+    public Text player1TurnPrompt;
+    public Text player2TurnPrompt;
     
     private PlayerController player1Controller;
     private PlayerController player2Controller;
     private TileGenerator tileGenerator;
     private float turnTimer;
+
+    private float fadeTimePassed;
+    private string fadeAnimationState;
+
+    private bool coroutineStarted;
+
+    private Color player1TurnPromptOriginalColor;
+    private Color player1TurnPromptEndColor;
+    private Color player2TurnPromptOriginalColor;
+    private Color player2TurnPromptEndColor;
 
     public enum BattleStates
     {
@@ -40,10 +55,22 @@ public class BattleStateManager : MonoBehaviour {
         tileGenerator = GetComponent<TileGenerator>();
 		currentState = BattleStates.GenerateMap;
         turnTimer = setTurnTimer;
+        fadeAnimationState = "Fade In";
+        fadeTimePassed = 0f;
     }
-	
-	private void Update () {
+
+    private void Start()
+    {
+        player1TurnPromptOriginalColor = player1TurnPrompt.color;
+        player1TurnPromptEndColor = new Color(player1TurnPrompt.color.r, player1TurnPrompt.color.g, player1TurnPrompt.color.b, 1);
+
+        player2TurnPromptOriginalColor = player2TurnPrompt.color;
+        player2TurnPromptEndColor = new Color(player2TurnPrompt.color.r, player2TurnPrompt.color.g, player2TurnPrompt.color.b, 1);
+    }
+
+    private void Update () {
         print(currentState);
+
         // Checks if game is over
         if (checkedPlayersDead() && checkIfInitialStates())
         {
@@ -110,9 +137,39 @@ public class BattleStateManager : MonoBehaviour {
                 break;
             case (BattleStates.StartPlayer1Turn):
                 // Start Player 1 Turn
-                player1Controller.startTurn();
+                if (fadeAnimationState == "Fade In")
+                {
+                    fadeTimePassed += Time.deltaTime;
+                    player1TurnPrompt.color = Color.Lerp(player1TurnPromptOriginalColor, player1TurnPromptEndColor, fadeTimePassed / promptFadeDuration);
+                }
+                else if (fadeAnimationState == "Display Prompt")
+                {
+                    fadeTimePassed += Time.deltaTime;
+                }
+                else if (fadeAnimationState == "Fade Out")
+                {
+                    fadeTimePassed += Time.deltaTime;
+                    player1TurnPrompt.color = Color.Lerp(player1TurnPromptEndColor, player1TurnPromptOriginalColor, fadeTimePassed / promptFadeDuration);
+                }
 
-                currentState = BattleStates.Player1Turn;
+                if (fadeTimePassed > promptFadeDuration && fadeAnimationState == "Fade In")
+                {
+                    fadeAnimationState = "Display Prompt";
+                    fadeTimePassed = 0f;
+                }
+                else if (fadeTimePassed > promptTime && fadeAnimationState == "Display Prompt")
+                {
+                    fadeAnimationState = "Fade Out";
+                    fadeTimePassed = 0f;
+                }
+                else if (fadeTimePassed > promptFadeDuration && fadeAnimationState == "Fade Out")
+                {
+                    player1Controller.startTurn();
+                    fadeAnimationState = "Fade In";
+                    fadeTimePassed = 0f;
+
+                    currentState = BattleStates.Player1Turn;
+                }
                 break;
             case (BattleStates.Player1Turn):
                 turnTimer -= Time.deltaTime;
@@ -131,9 +188,39 @@ public class BattleStateManager : MonoBehaviour {
                 break;
             case (BattleStates.StartPlayer2Turn):
                 // Start Player 2 Turn
-                player2Controller.startTurn();
+                if (fadeAnimationState == "Fade In")
+                {
+                    fadeTimePassed += Time.deltaTime;
+                    player2TurnPrompt.color = Color.Lerp(player2TurnPromptOriginalColor, player2TurnPromptEndColor, fadeTimePassed / promptFadeDuration);
+                }
+                else if (fadeAnimationState == "Display Prompt")
+                {
+                    fadeTimePassed += Time.deltaTime;
+                }
+                else if (fadeAnimationState == "Fade Out")
+                {
+                    fadeTimePassed += Time.deltaTime;
+                    player2TurnPrompt.color = Color.Lerp(player2TurnPromptEndColor, player2TurnPromptOriginalColor, fadeTimePassed / promptFadeDuration);
+                }
 
-                currentState = BattleStates.Player2Turn;
+                if (fadeTimePassed > promptFadeDuration && fadeAnimationState == "Fade In")
+                {
+                    fadeAnimationState = "Display Prompt";
+                    fadeTimePassed = 0f;
+                }
+                else if (fadeTimePassed > promptTime && fadeAnimationState == "Display Prompt")
+                {
+                    fadeAnimationState = "Fade Out";
+                    fadeTimePassed = 0f;
+                }
+                else if (fadeTimePassed > promptFadeDuration && fadeAnimationState == "Fade Out")
+                {
+                    player2Controller.startTurn();
+                    fadeAnimationState = "Fade In";
+                    fadeTimePassed = 0f;
+
+                    currentState = BattleStates.Player2Turn;
+                }
                 break;
             case (BattleStates.Player2Turn):
                 turnTimer -= Time.deltaTime;
@@ -159,6 +246,9 @@ public class BattleStateManager : MonoBehaviour {
                     player2Controller.endTurn();
                 }
 
+                player1TurnPrompt.color = player1TurnPromptOriginalColor;
+                player2TurnPrompt.color = player2TurnPromptOriginalColor;
+
                 Debug.Log("VICTORY");
                 break;
         }
@@ -172,5 +262,10 @@ public class BattleStateManager : MonoBehaviour {
     public bool checkIfInitialStates()
     {
         return (currentState != BattleStates.GenerateMap && currentState != BattleStates.SpawnPlayer1 && currentState != BattleStates.SpawnPlayer2);
+    }
+
+    IEnumerator PromptPlayerTurn()
+    {
+        yield return new WaitForSeconds(promptFadeDuration);
     }
 }
